1f248793697dd211798b6d47aa861210fcdb3b85
jradeen <omar.aljr41@gmail.com>
2025-05-15 21:50:18 +0300

    states.json

--- app/src/main/java/com/example/greengeeksweatherapp/Domains/CityData.kt
+++ app/src/main/java/com/example/greengeeksweatherapp/Domains/CityData.kt
@@ -1,3 +1,6 @@
 package com.example.greengeeksweatherapp.Domains
 
-data class CityData(val Name:String)
+data class CityData(
+    val Name:String,
+    val latitude: String,
+    val longitude: String)
